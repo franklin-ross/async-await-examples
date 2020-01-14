c4f331066e8017e42c4359fb5de5a6bfa4636f75
Franklin Ross <fhross@gmail.com>
2020-01-14 20:17:29 +1100

    Add dotnet project

---
+++ cs/00-basic.cs
@@ -0,0 +1,15 @@
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace AsyncAwaitExamples
+{
+    public class Basic
+    {
+
+        [Test]
+        public async Task Bob()
+        {
+
+        }
+    }
+}
