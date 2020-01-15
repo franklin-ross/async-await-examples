3f562f79524112f5503a7defa6910c7a108389c6
Franklin Ross <Frank.Ross@myob.com>
2020-01-15 16:48:03 +1100

    Add example to show similarities between async/await and yield return.

--- cs/20-StateMachine.cs
+++ cs/20-StateMachine.cs
@@ -1,15 +1,42 @@
 using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using NUnit.Framework;
 
 namespace AsyncAwaitExamples
 {
+    /// <summary>
+    /// This example shows two cases of using language features that yield execution to the caller,
+    /// and can then return back to the yield point later. `yield return` has been around longer
+    /// than `await` and seems better understood, but the concept is very similar.
+    /// </summary>
     public class StateMachinesTests
     {
+        [Test]
         public async Task AsyncStateMachine()
         {
+            Console.WriteLine("Hi");
+            await Task.Delay(1000);
+            Console.WriteLine("Hi");
+            await Task.Delay(1000);
+            Console.WriteLine("Hi");
+        }
 
+        [Test]
+        public void EnumerableStateMachine()
+        {
+            IEnumerable<int> EnumerableStateMachine()
+            {
+                Console.WriteLine("Hi");
+                yield return 1000;
+                Console.WriteLine("Hi");
+                yield return 1000;
+                Console.WriteLine("Hi");
+            }
+
+            foreach (var timeout in EnumerableStateMachine())
+                Thread.Sleep(timeout);
         }
     }
 }
