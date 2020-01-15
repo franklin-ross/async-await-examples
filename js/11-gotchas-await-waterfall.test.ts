5e0f834b4eb6cd039ae98aca9c16dffc89f17097
Franklin Ross <Frank.Ross@myob.com>
2020-01-15 17:18:23 +1100

    Add more parallel examples

--- js/11-gotchas-await-waterfall.test.ts
+++ js/11-gotchas-await-waterfall.test.ts
@@ -3,7 +3,7 @@ import { forceAsync } from "./utils";
 const loadFromServer = forceAsync;
 const saveToServer = forceAsync as (body: any) => Promise<number>;
 
-it("Do await Promise.all() for concurrent operations", async () => {
+it("Do await Promise.all() for parallel operations", async () => {
   const [load0, load1] = await Promise.all([
     loadFromServer(),
     loadFromServer()
@@ -12,9 +12,20 @@ it("Do await Promise.all() for concurrent operations", async () => {
   await saveToServer(combined);
 });
 
-it("Don't await promises in a waterfall if they could be concurrent", async () => {
-  const load0 = await loadFromServer();
-  const load1 = await loadFromServer();
-  const combined = [load0, load1];
+it("These will also run in parallel", async () => {
+  const load0 = loadFromServer();
+  const load1 = loadFromServer();
+  // Even though we're awaiting them individually, the operations themselves are already in flight
+  // and happening in parallel. `load1` may very well complete first, but the result will sit there
+  // until we await it.
+  const result0 = await load0;
+  const result1 = await load1;
+  await saveToServer([result0, result1]);
+});
+
+it("Await in serial if you need earlier results for later requests", async () => {
+  const loadFirst = await loadFromServer();
+  const loadNext = await loadFromServer(loadFirst);
+  const combined = [loadFirst, loadNext];
   await saveToServer(combined);
 });
